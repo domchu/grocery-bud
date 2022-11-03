109b991e284bd3c44656232a47750f85838f26c0
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-11-03 23:43:25 +0100

    chore: alert the success and danger message

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
-const Alert = () => {
-  return <h2>alert component</h2>;
+const Alert = ({ type, msg }) => {
+  return <p className={`alert alert-${type}`}>{meg}</p>;
 };
 
 export default Alert;
