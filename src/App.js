08099f2f267b3825665fbd1c31d8302112299b6b
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-11-19 23:54:46 +0100

    chore:added the delete item function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import List from "./List";
 import Alert from "./Alert";
-import { type } from "@testing-library/user-event/dist/type";
 
 function App() {
   const [name, setName] = useState("");
@@ -23,6 +22,7 @@ function App() {
       // deal with editID
     } else {
       // show alert
+      showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
@@ -33,10 +33,19 @@ function App() {
     setAlert({ show, type, msg });
   };
 
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
+  const removeItem = () => {
+    showAlert(true, "danger", "item removed ");
+    setList(list.filter((item) => item.id !== id));
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>Grocery Bud</h3>
         <div className="form-control">
           <input
@@ -53,8 +62,10 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
-          <button className="clear-btn">Clear items</button>
+          <List items={list} removeItem={removeItem} />
+          <button className="clear-btn" onClick={clearList}>
+            Clear items
+          </button>
         </div>
       )}
     </section>
--- src/List.js
+++ src/List.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = ({ items }) => {
+const List = ({ items, removeItem }) => {
   return (
     <div className="grocery-list">
       {items.map((item) => {
         const { id, title } = item;
         return (
-          <article className="grocery-item" key={id}>
+          <article key={id} className="grocery-item">
             <p className="title">{title} </p>
             <div className="btn-container">
               <button type="button" className="edit-btn">
                 <FaEdit />
               </button>
-              <button type="button" className="delete-btn">
+              <button
+                type="button"
+                className="delete-btn"
+                onClick={() => removeItem(id)}
+              >
                 <FaTrash />
               </button>
             </div>
